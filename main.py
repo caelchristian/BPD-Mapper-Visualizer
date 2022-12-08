ef8cf86a78ff29e40def3b20bcbb8825d1d2a383
caelchristian <caelchristian23@gmail.com>
2022-12-07 21:53:04 -0500

    ok last commit i promise

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from final import create_app, show_plots
 app = create_app()
 
 if __name__ == '__main__':
-    show_plots()
+    # GRADERS! Please uncomment this to see the plots ty
+    # show_plots()
+    
     app.run(debug=True)
     
